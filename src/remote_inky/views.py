e66f7716f51dd9a2ea6657dee2fa8ddd8d3cb31b
Axel Örn <dev@absalon.is>
2021-04-05 20:43:26 +0100

    Add view to upload photos to the inky

--- src/remote_inky/views.py
+++ src/remote_inky/views.py
@@ -1,8 +1,41 @@
-from flask import Blueprint
+import os
+from typing import Tuple
+
+from PIL import Image
+from flask import Blueprint, request
+from flask.globals import current_app
+from werkzeug.utils import secure_filename
 
 bp = Blueprint("inky", __name__, url_prefix="")
+
+ALLOWED_EXTENSIONS = ("jpg", "jpeg", "png")
 
 
 @bp.route("/", methods=("GET",))
 def home() -> str:
     return "Hello!"
+
+
+@bp.route("/image", methods=("POST",))
+def image() -> Tuple[str, int]:
+    if "file" not in request.files:
+        return "File missing", 419
+    file = request.files["file"]
+
+    if file.filename == "":
+        return "No selected file", 419
+
+    extension = file.filename.split(".")[-1].lower()
+    if extension not in ALLOWED_EXTENSIONS:
+        return f"Extension ({extension}) not supported", 403
+
+    filename = secure_filename(file.filename)
+    file_path = os.path.join("/tmp", filename)
+    file.save(file_path)
+
+    image = Image.open(file_path)
+    inky = current_app.config["inky"]
+    inky.set_image(image, saturation=0.5)
+    inky.show()
+
+    return "Updated!", 200
